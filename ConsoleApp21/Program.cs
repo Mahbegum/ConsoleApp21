8af10ba1d5f87acdc7fea553fe1a387a185aaf1e
Mahbegum <mahbegum25@gmail.com>
2023-07-02 22:29:40 -0700

    Add project files.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employee empl1 = new employee();
+            empl1.Id = 1;
+            empl1.FirstName = "Raziq";
+            empl1.LastName = "Ali";
+
+            employee empl2 = new employee();
+            empl2.Id = 2;
+            empl2.LastName = "Raziq";
+            empl2.LastName = "Ali";
+
+            // comparing two inputs 
+            Console.WriteLine(empl1 == empl2);
+            Console.WriteLine(empl2 != empl1);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp21/employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class employee
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // To check if two inputs are the same
+
+        public static bool operator ==(employee empl1, employee empl2)
+        {
+            if (ReferenceEquals(empl1, empl2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public static bool operator !=(employee empl1, employee empl2)
+        {
+            if (!ReferenceEquals(empl1, empl2))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
